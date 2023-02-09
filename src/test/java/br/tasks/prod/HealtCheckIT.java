6f628dad9dff63cc118cc26f527a3e3e06320708
vinizuppa <vcordeiro12@gmail.com>
2023-02-09 14:09:42 -0300

    Health Check

---
+++ src/test/java/br/tasks/prod/HealtCheckIT.java
@@ -0,0 +1,30 @@
+package br.tasks.prod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class HealtCheckIT {
+	@Test
+	public void healtCheck() throws MalformedURLException {
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.110:4444/wd/hub"), cap);
+		try {
+			driver.navigate().to("http://192.168.1.110:9999/tasks/");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			String version = driver.findElement(By.id("version")).getText();
+			Assert.assertTrue(version.startsWith("build"));
+		} finally {
+			driver.quit();
+		}
+		
+		
+	}
+}
